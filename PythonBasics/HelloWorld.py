2fb279316091bba5d16c29a1145d7d150b0e1b89
Harsh <harshmahendru802@gmail.com>
2026-03-17 20:59:54 -0400

    use of List and other basic py concepts

---
+++ PythonBasics/HelloWorld.py
@@ -0,0 +1,22 @@
+print("Hello World")
+# comments
+
+#variables
+a = 3
+print(a)
+
+string = "Hey There!"
+print(string)
+b,c,d = 3,5.2, "Wassup!"
+print("Value is",b,"and second is",c,"and last",d)
+
+#list / array
+values = [1, 2 , "Me H.J.M", 5]
+
+
+print(values[2])
+print(values[-1])
+values.insert(4, "No way")
+print(values[4])
+values.append(33)
+print(values)
---
+++ README.md
@@ -0,0 +1,4 @@
+# QA-Playwrite
+I will be learing Python Playwrite.
+
+I have to use ".\.venv\Scripts\activate" cmd when i Open New Terminal.
