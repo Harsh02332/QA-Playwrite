e9666e721ea5e7cf3ad6764be678bb7ba07bba78
Harsh <harshmahendru802@gmail.com>
2026-03-18 22:49:57 -0400

    changes in Loop.py

--- PythonBasics/Loop.py
+++ PythonBasics/Loop.py
@@ -1,7 +1,11 @@
+a=18
 greet = "Good Morning"
 if greet == "Good Morning":
     print("Condition Matched")
-    print("Second Line")
+    if a<3:
+        print("its match")
+    elif a>13:
+        print("Still Grater then",a)
 else:
     print("Doesn't match")
 print("Condition checked")
