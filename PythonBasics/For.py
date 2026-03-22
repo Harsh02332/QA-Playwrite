1a6c253e9b28da16b8013960a64464cfe760704d
Harsh <harshmahendru802@gmail.com>
2026-03-22 19:51:22 -0400

    update in For.py

--- PythonBasics/For.py
+++ PythonBasics/For.py
@@ -9,5 +9,9 @@ for j in range(1, 6): # in range (i,j) -> [i to j-1]
     sum = sum + j
 print(sum)
 print("__________________________________________________________")
-for k in range(1, 12, 2):
+for k in range(1, 12, 2): # this will add difference of 2
     print(k)
+print("___________________________Skip First Index_______________________________")
+for H in range(12): # this will add difference of 2
+    print(H)
+print("End")
