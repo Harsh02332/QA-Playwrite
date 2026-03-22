aee336f7e88f28a8efa85de9fc3f64698333a797
Harsh <harshmahendru802@gmail.com>
2026-03-22 19:49:41 -0400

    added file name For.py

---
+++ PythonBasics/For.py
@@ -0,0 +1,13 @@
+obj = [3,13,53,12,23]
+
+for i in obj :
+    print(i*5)
+
+# Sum of 5 numbers
+sum = 0
+for j in range(1, 6): # in range (i,j) -> [i to j-1]
+    sum = sum + j
+print(sum)
+print("__________________________________________________________")
+for k in range(1, 12, 2):
+    print(k)
