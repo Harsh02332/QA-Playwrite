7ab71f17126cbcefd3bc23039ac6ac4042d40ba4
Harsh <harshmahendru802@gmail.com>
2026-03-26 20:49:03 -0400

    fixture(scope="function")

--- PytestPython/TestPyTestValidation.py
+++ PytestPython/TestPyTestValidation.py
@@ -1,9 +1,12 @@
 #fixture
 import pytest
-@pytest.fixture
+@pytest.fixture(scope="function")
 def preWork():
     print("I Setup browser instance")
 
 
 def test_initialCheck(preWork):
     print("This is First Test")
+
+def test_secondCheck(preWork):
+    print("This is Second Test")
